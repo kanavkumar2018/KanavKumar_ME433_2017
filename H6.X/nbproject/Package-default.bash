5de6cd559741e38ad02b878415d611df9825be4a
kanavkumar2018 <kanavkumar@Kanavs-MacBook-Pro.local>
2017-05-06 17:38:24 -0500

    HW6

--- H6.X/nbproject/Package-default.bash
+++ H6.X/nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/H6_1.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=H6_1.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=h61.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/H6.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=H6.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=h6.x/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/h61.x/bin
+makeDirectory ${TMPDIR}/h6.x/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/h61.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/h6.x.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/h61.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/h6.x.tar *
 checkReturnCode
 
 # Cleanup
